4a30ae8855453a8228289293163395a9a8f04069
lillianstubbs01 <lillianstubbs01@comcast.net>
2023-11-08 12:35:15 -0500

    Updating Details Page

--- nextjs/src/app/details/page.js
+++ nextjs/src/app/details/page.js
@@ -1,18 +1,40 @@
+'use client'
+
 import React from 'react';
 import NavBar from '../components/navbar/navbar';
 import DetailsCard from '../components/details-card/details-card';
 import Link from 'next/Link';
+import Image from 'next/Image';
+import { HomeRegular, BedRegular, VehicleCarRegular, OvenRegular, CalendarRegular, ArrowRightRegular } from '@fluentui/react-icons';
+import walkWayImgSrc from '../assets/fence_walk_way.webp';
+
+import "./details.css"
 
 function Details() {
     // Constants for Property Details Card
-    const propertyDetailsList = <ul>
-        <li>Single Family Home</li>
-        <li>3 Bedroom, 2 Bathroom</li>
-        <li>8 Parking Spaces</li>
-        <li>Dishwasher, Double Oven, Gas Water Heater</li>
-        <li>Built in 2012</li>
-    </ul>;
-    const propertyDetailsDescription = <div>
+    const propertyDetailsList = <div className="property-details-list smaller-content">
+        <div className="icon-row">
+            <HomeRegular fontSize={40} style={{"color": "8C310F"}}/>
+            <p>Single Family Home</p>
+        </div>
+        <div className="icon-row">
+            <BedRegular fontSize={40} style={{"color": "8C310F"}}/>
+            <p>3 Bedroom, 2 Bathroom</p>
+        </div>
+        <div className="icon-row">
+            <VehicleCarRegular fontSize={40} style={{"color": "8C310F"}}/>
+            <p>8 Parking Spaces</p>
+        </div>
+        <div className="icon-row">
+            <OvenRegular fontSize={40} style={{"color": "8C310F"}}/>
+            <p>Dishwasher, Double Oven, Gas Water Heater</p>
+        </div>
+        <div className="icon-row">
+            <CalendarRegular fontSize={40} style={{"color": "8C310F"}}/>
+            <p>Built in 2012</p>
+        </div>
+    </div>;
+    const propertyDetailsDescription = <div className='details-text larger-content'>
         <p>Welcome to your dream home! This one-of-a-kind property offers the perfect combination of waterfront living and craftsmanship. An impressive 21.127 private acres, partially fenced, with rolling pastures and two stocked ponds with fountains make this a truly special place for you to call home. </p>
         <p>Enjoy fishing off your back porch or deer hunting at its finest on your own land. Inside you will find top of line appliances paired with 2 huge gas burning fireplaces perfect for cozy nights at home. With vaulted ceilings this 3 bedroom 3 full bath home is sure to impress even the most discerning buyer. </p>
         <p>Additional features include a 3 car garage plus 2 car detached garage with shop as well as an upstairs man cave above the detached garage - perfect for weekend getaways without ever leaving home! And that's not all - this property also boasts 500 feet of creek along its property line making it ideal for those who love spending time outdoors or even a mini horse farm! Live life on the water in beautiful surroundings - don't miss out on this rare opportunity - schedule a showing today!</p>
@@ -22,12 +44,14 @@ function Details() {
     const api_key = "AIzaSyALgvI2svq7rRtxR-1yMkHxTTHn4eObOaM";
     const map_mode = "place";
     const map_params = "q=Homer,GA+United+States";
-    const locationDescription = <p>Location Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Duis semper gravida ex, lacinia cursus sapien feugiat ac. Pellentesque eu ligula risus. Praesent in quam erat.
-        Maecenas pharetra leo eget euismod pretium. In hac habitasse platea dictumst.<b>Homer GA, 30547</b></p>;
+    const locationDescription = <div className='smaller-content details-text'>
+        <p>Location Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Duis semper gravida ex, lacinia cursus sapien feugiat ac. Pellentesque eu ligula risus. Praesent in quam erat.
+            Maecenas pharetra leo eget euismod pretium. In hac habitasse platea dictumst.</p>
+        <p><b>Homer GA, 30547</b></p>
+    </div>;
     const map = <iframe
-        width="450"
-        height="250"
+        className="larger-content"
         frameBorder="0" style={{"border": "0"}}
         referrerPolicy="no-referrer-when-downgrade"
         src={`https://www.google.com/maps/embed/v1/${map_mode}?key=${api_key}&${map_params}`}
@@ -41,21 +65,37 @@ function Details() {
     </p>;
 
     // Constants for Jump Start Your Vacation Card
-    const redirectButtons = <div>
+    const redirectButtons = <div className='right-content details-text'>
         <p>To see our pricing check our availabilities. </p>
-        <Link href='/availabilities'>
-            <button>See Availabilities</button>
+        <Link href='/availabilities' className='link-decoration'>
+            <div className='icon-row arrow-btn'>
+                <div>See Availabilities</div>
+                <ArrowRightRegular fontSize={30} style={{"color": "#261813"}}/>
+            </div>
         </Link>
         <p>For more pictures of house try our photo gallery</p>
-        <Link href='/photo-gallery'>
-            <button>Go To Photo Gallery</button>
+        <Link href='/photo-gallery' className='link-decoration'>
+            <div className='icon-row arrow-btn'>
+                <div>Go To Photo Gallery</div>
+                <ArrowRightRegular fontSize={30} style={{"color": "#261813"}}/>
+            </div>
         </Link>
         <p>Have more questions? Get in touch with us. We are always happy to help.</p>
-        <Link href='/contact'>
-            <button>Contact Now</button>
+        <Link href='/contact' className='link-decoration'>
+            <div className='icon-row arrow-btn'>
+                <div>Contact Now</div>
+                <ArrowRightRegular fontSize={30} style={{"color": "#261813"}}/>
+            </div>
         </Link>
     </div>;
-    const vacationPhoto = <div>Insert Image Here</div>;
+    const vacationPhoto = <div className='larger-content'>
+        <Image src={walkWayImgSrc}
+               alt="A view of the home from the drive up to the house. You can see the fence leading up to the house,
+               the detached garage, and the pond with a fountain bursting out."
+               layout='fill'
+               objectFit='contain'
+        />
+    </div>;
 
     return (
         <div>
