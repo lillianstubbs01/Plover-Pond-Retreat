f5317f4c15aab2486ed1ccd97a7d6f382d147365
lillianstubbs01 <lillianstubbs01@comcast.net>
2024-03-27 00:03:08 -0400

    Finish Contact CSS

--- nextjs/src/app/availabilities/page.js
+++ nextjs/src/app/availabilities/page.js
@@ -1,7 +1,21 @@
+import React from 'react';
+import Navbar from "../components/navbar/navbar";
+
 function Availabilities() {
     return (
         <div>
-            <h1>Availabilities</h1>
+            <Navbar/>
+            <div>
+                <h1>Availabilities</h1>
+                {/*<div className="airbnb-embed-frame" data-id="898714721319697343" data-view="home" data-hide-price="true"*/}
+                {/*     style={{"width": "450px", "height": "300px", "margin": "auto"}}><a*/}
+                {/*    href="https://www.airbnb.com/rooms/898714721319697343?check_in=2024-04-01&amp;check_out=2024-04-06&amp;guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget">View*/}
+                {/*    On Airbnb</a><a*/}
+                {/*    href="https://www.airbnb.com/rooms/898714721319697343?check_in=2024-04-01&amp;check_out=2024-04-06&amp;guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget"*/}
+                {/*    rel="nofollow">Home in Homer · ★4.95 · 3 bedrooms · 6 beds · 3 baths</a>*/}
+                {/*    <script async="" src="https://www.airbnb.com/embeddable/airbnb_jssdk"></script>*/}
+                {/*</div>*/}
+            </div>
         </div>
     )
 }
