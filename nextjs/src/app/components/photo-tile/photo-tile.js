2ca7683f822e0c9e6251ac6e5a97bc581d98d8f4
lstubbs8 <lstubbs8@gatech.edu>
2024-03-31 20:21:16 -0400

    Completed PhotoGallery
    Added photo gallery functionality, disabled tabbing behind modal, made css-based modal, added url search parameters to open photos

--- nextjs/src/app/components/photo-tile/photo-tile.js
+++ nextjs/src/app/components/photo-tile/photo-tile.js
@@ -2,17 +2,35 @@
 
 import React from 'react';
 import Image from 'next/image';
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
+
 
 import './photo-tile.css'
 
 function PhotoTile(props) {
-    return <div className='image' onClick={() => props.view(true)}>
-        <Image
+    let searchParams = useSearchParams();
+    const path = usePathname();
+    const { replace } = useRouter();
+
+    function handleParams(photoId) {
+        let params = new URLSearchParams(searchParams);
+        if (photoId >= 0) {
+            params.set('photoId', photoId);
+        }
+        replace(`${path}?${params}`);
+    }
+
+    return <button className='image' disabled={props.view} tabIndex={props.view ? -1 : 0} onClick={(e) => {
+        props.setOpenView(true);
+        handleParams(props.photoNumber);
+        props.setOpenImage(props.photoNumber);
+    }}>
+        <Image className='image-raw'
             src={props.source}
             alt={props.altText}
             height="20vw"
         />
-    </div>
+    </button>
 }
 
 export default PhotoTile;
--- nextjs/src/app/photo-gallery/page.js
+++ nextjs/src/app/photo-gallery/page.js
@@ -1,6 +1,9 @@
 'use client'
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { DismissRegular, ArrowRightRegular, ArrowLeftRegular } from '@fluentui/react-icons';
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
+import Image from 'next/image';
 import NavBar from '../components/navbar/navbar';
 import PhotoTile from '../components/photo-tile/photo-tile';
 import imageTest from '../assets/birds_eye.webp';
@@ -10,17 +13,59 @@ import image3 from '../assets/master_bedroom_narrow.webp'
 import './photo-gallery.css'
 
 const allPhotos = [
-    {"key": 0, "source": imageTest, "altText": "This is image 1"},
-    {"key": 1, "source": image2, "altText": "This is image 2"},
-    {"key": 2, "source": image3, "altText": "This is image 3"},
-    {"key": 3, "source": imageTest, "altText": "This is image 1"},
-    {"key": 4, "source": image2, "altText": "This is image 2"},
-    {"key": 5, "source": imageTest, "altText": "This is image 1"},
-    {"key": 6, "source": image2, "altText": "This is image 2"}
+    {"key": 0, "source": imageTest, "altText": "This is image 0. hallo"},
+    {"key": 1, "source": image2, "altText": "This is image 1"},
+    {"key": 2, "source": image3, "altText": "This is image 2"},
+    {"key": 3, "source": imageTest, "altText": "This is image 3"},
+    {"key": 4, "source": image2, "altText": "This is image 4"},
+    {"key": 5, "source": imageTest, "altText": "This is image 5"},
+    {"key": 6, "source": image2, "altText": "This is image 6"}
 ];
 
 function PhotoGallery() {
     const [openView, setOpenView] = useState(false);
+    const [openImage, setOpenImage] = useState(0);
+    const photoCount = allPhotos.length;
+    let searchParams = useSearchParams();
+    const path = usePathname();
+    const { replace } = useRouter();
+
+    useEffect(() => {
+        if (searchParams.get('photoId') !== null) {
+            let photoId = Number(searchParams.get('photoId'));
+            if (photoId >= 0 && photoId < allPhotos.length) {
+                setOpenImage(photoId);
+                setOpenView(true);
+            }
+        }
+    }, [searchParams])
+
+    function handleParams(photoId) {
+        let params = new URLSearchParams(searchParams);
+        if (photoId >= 0) {
+            params.set('photoId', photoId);
+        } else {
+            params.delete('photoId');
+        }
+        replace(`${path}?${params}`);
+    }
+
+    function handleArrowRight(photoId) {
+        if (photoId < photoCount - 1) {
+            handleParams(photoId + 1);
+        }
+    }
+
+    function handleArrowLeft(photoId) {
+        if (photoId > 0) {
+            handleParams(photoId - 1);
+        }
+    }
+
+    function handleClose() {
+        setOpenView(false);
+        replace(`${path}`)
+    }
 
     return (
         <div className='photo-gallery-page'>
@@ -33,12 +78,28 @@ function PhotoGallery() {
                         photoNumber={photo.key}
                         source={photo.source}
                         altText={photo.altText}
-                        view={setOpenView}
+                        setOpenView={setOpenView}
+                        view={openView}
+                        setOpenImage={setOpenImage}
+
                     />
                 })}
             </div>
             <div className={openView ? 'photo-view' : 'hidden'}>
-                <h1>HERE</h1>
+                <button className="close photo-button" onClick={() => handleClose()}>
+                    <DismissRegular className="photo-icon"/>
+                </button>
+                <button className="photo-button arrow-left" onClick={() => handleArrowLeft(openImage)}>
+                    <ArrowLeftRegular className="photo-icon"/>
+                </button>
+                <Image className="open-image"
+                       src={allPhotos[openImage].source}
+                       alt={allPhotos[openImage].altText}
+                />
+                <button className="photo-button arrow-right" onClick={() => handleArrowRight(openImage)}>
+                    <ArrowRightRegular className="photo-icon"/>
+                </button>
+                <p className="counter">{openImage + 1}/{photoCount}</p>
             </div>
         </div>
     );
