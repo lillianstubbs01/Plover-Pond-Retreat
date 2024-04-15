76b14eb7f0009f9bdcce9056e5d131df4d02b78c
lstubbs8 <lstubbs8@gatech.edu>
2024-04-15 13:56:11 -0400

    Update Emailing Service Headers

--- src/app/services/Messaging/messaging.js
+++ src/app/services/Messaging/messaging.js
@@ -2,6 +2,13 @@ import { apiHandler } from '../api/api';
 
 const args = 'emailservice';
 // ApiHandler(request, args, headers={}, content={})
+const emailHeaders = {
+    'Content-Type': '',
+    'Accept': '',
+    'Authorization': '',
+    'User-Agent': '',
+    'Access-Control-Allow-Origin': 'https://lillianstubbs01.github.io'
+};
 
 export const messagingService = async (name, email, message) => {
     const messageBody = {
@@ -10,6 +17,6 @@ export const messagingService = async (name, email, message) => {
         "message": message
     }
 
-    let res = apiHandler("post", args, {}, messageBody);
+    let res = apiHandler("post", args, emailHeaders, messageBody);
     return res;
 }
